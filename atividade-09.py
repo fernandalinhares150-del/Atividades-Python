7b877aadee9fa8e97d6d17f5b2760c2dde053bed
FERNANDA DE ALMEIDA LINHARES <fernanda_linhares150@estudante.sesisenai.org.br>
2026-09-24 16:46:02 -0300

    primeiro commit

---
+++ atividade-09.py
@@ -0,0 +1,17 @@
+nome = input("Digite o nome do aluno: ")
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+
+print("Aluno:", nome)
+print("Média:", media)
+
+if media >= 7:
+    print("Aprovado")
+elif media >= 5:
+    print("Recuperação")
+else:
+    print("Reprovado")
